d25f01e4fb41cb714d802169823f9dd5a839f639
rahulBharagv71 <rahul.20225071@mnnit.ac.in>
2024-06-25 19:33:18 +0530

    deployment debugg

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -3,9 +3,9 @@ import { stripe } from "@/lib/stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
-import { Resend } from "resend";
-import OrderReceivedEmail from "@/components/emails/order-recived-email";
-const resend = new Resend(process.env.RESEND_API_KEY);
+//import { Resend } from "resend";
+//import OrderReceivedEmail from "@/components/emails/order-recived-email";
+//const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
@@ -66,31 +66,31 @@ export async function POST(req: Request) {
           },
         },
       });
-      resend.emails
-        .send({
-          from: "CaseCobra <onboarding@resend.dev>",
-          to: [event.data.object.customer_details.email],
-          subject: "Thanks for your order!",
-          react: OrderReceivedEmail({
-            orderId,
-            orderDate: updatedOrder.createdAt.toLocaleDateString(),
-            // @ts-ignore
-            shippingAddress: {
-              name: session.customer_details!.name!,
-              city: shippingAddress?.city!,
-              country: shippingAddress?.country!,
-              postalCode: shippingAddress?.postal_code!,
-              street: shippingAddress?.line1!,
-              state: shippingAddress?.state!,
-            },
-          }),
-        })
-        .then(() => {
-          console.log("Email sent successfully!");
-        })
-        .catch((error: Error) => {
-          console.error("Failed to send email:", error);
-        });
+      // resend.emails
+      //   .send({
+      //     from: "CaseCobra <onboarding@resend.dev>",
+      //     to: [event.data.object.customer_details.email],
+      //     subject: "Thanks for your order!",
+      //     react: OrderReceivedEmail({
+      //       orderId,
+      //       orderDate: updatedOrder.createdAt.toLocaleDateString(),
+      //       // @ts-ignore
+      //       shippingAddress: {
+      //         name: session.customer_details!.name!,
+      //         city: shippingAddress?.city!,
+      //         country: shippingAddress?.country!,
+      //         postalCode: shippingAddress?.postal_code!,
+      //         street: shippingAddress?.line1!,
+      //         state: shippingAddress?.state!,
+      //       },
+      //     }),
+      //   })
+      //   .then(() => {
+      //     console.log("Email sent successfully!");
+      //   })
+      //   .catch((error: Error) => {
+      //     console.error("Failed to send email:", error);
+      //   });
     }
 
     return NextResponse.json({ resul: event, ok: true });
