6e56039c799d86985f0075e136e41c75d33fdcad
rahulBharagv71 <rahul.20225071@mnnit.ac.in>
2024-06-25 19:34:17 +0530

    debugg commit

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -68,7 +68,7 @@ export async function POST(req: Request) {
       });
       // resend.emails
       //   .send({
-      //     from: "CaseCobra <onboarding@resend.dev>",
+      //     from: "Casexpress <onboarding@resend.dev>",
       //     to: [event.data.object.customer_details.email],
       //     subject: "Thanks for your order!",
       //     react: OrderReceivedEmail({
